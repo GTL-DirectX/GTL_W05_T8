700778b0da08ff0ce9b99fea24de215fd132a2de
gudtldn <gudtldn1234@gmail.com>
2025-04-09 21:47:24 +0900

    wip: SceneManager에 JSON 직렬화 및 역직렬화 기능 추가

--- EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/SceneManager.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/SceneManager.cpp
@@ -1,13 +1,64 @@
 ﻿#include "SceneManager.h"
+
+#include "EditorViewportClient.h"
 #include "Container/Array.h"
 #include "Container/Map.h"
+#include "Engine/EditorEngine.h"
+#include "Engine/Engine.h"
 #include "Math/Vector.h"
 #include "UObject/NameTypes.h"
 
 #include "JSON/json.hpp"
+#include "LevelEditor/SLevelEditor.h"
 
 using json = nlohmann::json;
 
+
+#pragma region nlohmann::json function overload
+[[maybe_unused]]
+static void to_json(json& Json, const FVector& V)
+{
+    Json = json{{"X", V.X}, {"Y", V.Y}, {"Z", V.Z}};
+}
+
+[[maybe_unused]]
+static void to_json(json& Json, const FString& S)
+{
+    Json = S;
+}
+
+[[maybe_unused]]
+static void to_json(json& Json, const FName& N)
+{
+    Json = N.ToString();
+}
+
+[[maybe_unused]]
+static void from_json(const json& Json, FVector& V)
+{
+    V.X = Json.at("X").get<float>();
+    V.Y = Json.at("Y").get<float>();
+    V.Z = Json.at("Z").get<float>();
+}
+
+[[maybe_unused]]
+static void from_json(const json& Json, FString& S)
+{
+    if (Json.is_string())
+    {
+        S = Json.get<FString>();
+    }
+}
+
+[[maybe_unused]]
+static void from_json(const json& Json, FName& N)
+{
+    if (Json.is_string())
+    {
+        N = Json.get<FString>();
+    }
+}
+#pragma endregion
 
 namespace SceneManagerData
 {
@@ -18,6 +69,8 @@ struct FEditorCamaraData
     float FOVAngle;
     float NearClipPlane;
     float FarClipPlane;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(FEditorCamaraData, Location, Rotation, FOVAngle, NearClipPlane, FarClipPlane)
 };
 
 struct FTransformData
@@ -25,20 +78,26 @@ struct FTransformData
     FVector Location;
     FVector Rotation;
     FVector Scale;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(FTransformData, Location, Rotation)
 };
 
 struct FActorData
 {
+    FString ActorLabel;
     FName Type;
     FTransformData TransformData;
-    FString ActorLabel;
+    // TMap<FName, TPair<FString, FString>> Properties;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(FActorData, ActorLabel, Type, TransformData)
 };
 
 struct FLevelData
 {
     FString LevelName;
     TArray<FActorData> Actors;
-    TMap<FName, TPair<FString, FString>> Properties;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(FLevelData, LevelName, Actors)
 };
 
 struct FSceneData
@@ -46,5 +105,102 @@ struct FSceneData
     int32 Version;
     FEditorCamaraData EditorCamara;
     TArray<FLevelData> Levels;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(FSceneData, Version, EditorCamara, Levels)
 };
+}
+
+
+UWorld* FSceneManager::LoadSceneFromJsonFile(const FString& FilePath)
+{
+    // TODO: Implements This
+    return nullptr;
+}
+
+bool FSceneManager::SaveSceneToJsonFile(const FString& FilePath, const UWorld* World)
+{
+    const json Data = JsonToSceneData(FilePath);
+    FString JsonString = Data.dump(4); // 4는 들여쓰기 수준
+
+    return true;
+}
+
+SceneManagerData::FSceneData FSceneManager::JsonToSceneData(const FString& JsonString)
+{
+    return {};
+}
+
+SceneManagerData::FSceneData FSceneManager::WorldToSceneData(const UWorld* InWorld)
+{
+    using namespace SceneManagerData;
+
+    // 뷰포트의 카메라 정보를 저장 (현재는 1개만)
+#if 1 // TODO: WITH_EDITOR 추가하면 변경하기
+    const AEditorPlayer* EdPlayer = Cast<UEditorEngine>(GEngine)->GetEditorPlayer();
+    const auto FirstViewport = *GEngineLoop.GetLevelEditor()->GetViewports();
+    FEditorCamaraData CameraData =
+    {
+        .Location = EdPlayer->GetActorLocation(),
+        .Rotation = EdPlayer->GetActorRotation(),
+        .FOVAngle = FirstViewport->FOVAngle,
+        .NearClipPlane = FirstViewport->nearPlane,
+        .FarClipPlane = FirstViewport->farPlane,
+    };
+#else
+    FEditorCamaraData CameraData =
+    {
+        .Location = 0.0f,
+        .Rotation = 0.0f,
+        .FOVAngle = 0.0f,
+        .NearClipPlane = 0.1f,
+        .FarClipPlane = 10000.0f
+    };
+#endif
+
+    // 월드의 레벨 정보를 저장
+    const ULevel* ActiveLevel = InWorld->GetActiveLevel();
+
+    TArray<FActorData> ActorsData;
+    for (const AActor* Actor : ActiveLevel->Actors)
+    {
+        // TODO: 나중에 UPROPERTY를 이용해서 적용
+        // TMap<FName, TArray<TPair<FString, FString>>> ActorProperties;
+        // for (const UActorComponent* Comp : Actor->GetComponents())
+        // {
+        //     if (USceneComponent* SceneComp = Cast<USceneComponent>(Comp))
+        //     {
+        //         TArray<TPair<FString, FString>> Properties;
+        //         if (Comp->IsA<>())
+        //         {
+        //             
+        //         }
+        //         ActorProperties[Comp->GetFName()] = Properties;
+        //     }
+        // }
+
+        ActorsData.Emplace(
+#if 1 // TODO: WITH_EDITOR 추가하면 변경하기
+            Actor->GetActorLabel(),
+#else
+            Actor->GetName(),
+#endif
+            Actor->GetClass()->GetFName(),
+            FTransformData {
+                .Location = Actor->GetActorLocation(),
+                .Rotation = Actor->GetActorRotation(),
+                .Scale = Actor->GetActorScale(),
+            }
+        );
+    }
+
+    return {
+        .Version = 1,
+        .EditorCamara = CameraData,
+        .Levels = {
+            {
+                .LevelName = ActiveLevel->GetName(),
+                .Actors = ActorsData,
+            }
+        },
+    };
 }
--- EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/SceneManager.h
+++ EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/SceneManager.h
@@ -1,9 +1,62 @@
 ﻿#pragma once
+#include <memory>
 
+#include "HAL/PlatformType.h"
+#include "World/World.h"
+
+
+namespace SceneManagerData
+{
 struct FSceneData;
+}
+
+class FString;
 
 
 class FSceneManager
 {
-    
+public:
+    // TODO: AssetManager 만들면 Manager에서 필요한 값을 가져오는걸로 바꾸기
+
+    /**
+     * Json형식으로 저장된 World파일을 불러옵니다.
+     * @param FilePath Json형식으로 World정보가 저장된 파일의 경로
+     * @return 생성된 World
+     */
+    static UWorld* LoadSceneFromJsonFile(const FString& FilePath);
+
+    /**
+     * World를 Json형식으로 저장합니다.
+     * @param FilePath World를 저장할 파일 경로
+     * @param World 저장할 World
+     * @return 성공적으로 저장되었는지 여부
+     */
+    static bool SaveSceneToJsonFile(const FString& FilePath, const UWorld* World);
+
+private:
+    /**
+     * JSON 문자열을 역직렬화하여 FSceneData를 생성합니다.
+     *
+     * @param JsonString 역직렬화할 JSON 문자열
+     * @return 역직렬화된 FSceneData 객체
+     */
+    static SceneManagerData::FSceneData JsonToSceneData(const FString& JsonString);
+
+    /**
+     * World를 FSceneData로 직렬화합니다.
+     *
+     * @param InWorld 직렬화할 World 객체
+     * @return World를 표현하는 FSceneData 객체
+     */
+    static SceneManagerData::FSceneData WorldToSceneData(const UWorld* InWorld);
+
+private:
+    // TODO: IFileManager::Get().CreateFileReader() & Writer() 만들면 여기에 추가
+    // static void DeserializeFromBinary(const void* Data, int64 Size, SceneManagerData::FSceneData& OutSceneData);
+    // or 중간 구조체 사용 안할경우
+    // static UWorld* DeserializeFromBinary(const void* Data, int64 Size);
+
+    // static TArray<uint8> SerializeToBinary(const SceneManagerData::FSceneData& SceneData);
+    // or 중간 구조체 사용 안할경우
+    // static TArray<uint8> SerializeToBinary(UWorld* World);
 };
