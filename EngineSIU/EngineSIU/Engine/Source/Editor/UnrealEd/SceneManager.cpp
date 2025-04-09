31338cb41d7c1d4aaa6462394431ec0bbfb70132
gudtldn <gudtldn1234@gmail.com>
2025-04-09 15:13:24 +0900

    feat: SceneManager.cpp 및 SceneManager.h 파일 추가, FSceneData 구조체 정의

---
+++ EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/SceneManager.cpp
@@ -0,0 +1,50 @@
+﻿#include "SceneManager.h"
+#include "Container/Array.h"
+#include "Container/Map.h"
+#include "Math/Vector.h"
+#include "UObject/NameTypes.h"
+
+#include "JSON/json.hpp"
+
+using json = nlohmann::json;
+
+
+namespace SceneManagerData
+{
+struct FEditorCamaraData
+{
+    FVector Location;
+    FVector Rotation;
+    float FOVAngle;
+    float NearClipPlane;
+    float FarClipPlane;
+};
+
+struct FTransformData
+{
+    FVector Location;
+    FVector Rotation;
+    FVector Scale;
+};
+
+struct FActorData
+{
+    FName Type;
+    FTransformData TransformData;
+    FString ActorLabel;
+};
+
+struct FLevelData
+{
+    FString LevelName;
+    TArray<FActorData> Actors;
+    TMap<FName, TPair<FString, FString>> Properties;
+};
+
+struct FSceneData
+{
+    int32 Version;
+    FEditorCamaraData EditorCamara;
+    TArray<FLevelData> Levels;
+};
+}
